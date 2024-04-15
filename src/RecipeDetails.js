6a58448e71509935ffc9d6750cd1aa99ec9b5fce
VangavarapuAnitha <sscanitha123@gmail.com>
2024-04-15 08:21:09 +0530

    added styles to the RecipeDetails page

--- src/RecipeDetails.js
+++ src/RecipeDetails.js
@@ -7,8 +7,8 @@ import "./RecipeDetails.css";
 const RecipeDetails = () => {
   let params = useParams();
   const [details, setDetails] = useState({});
-  const [showInstructions,setShowInstructions]= useState(false);
-  const [showIngredients,setShowIngredients]= useState(false);
+  const [showInstructions, setShowInstructions] = useState(false);
+  const [showIngredients, setShowIngredients] = useState(false);
   const API_KEY = "236ad32b208a45f48724c6069ec39aea";
 
   useEffect(() => {
@@ -26,43 +26,61 @@ const RecipeDetails = () => {
     fetchDetails();
   }, [params.id]);
 
-  const handleInstruction=() =>{
+  const handleInstruction = () => {
     setShowInstructions(true);
     setShowIngredients(false);
-  }
-  const handleIngredients=() =>{
+  };
+  const handleIngredients = () => {
     setShowIngredients(true);
     setShowInstructions(false);
-    
-  }
+  };
   return (
     <div>
       <Link to="/" className="backToHome">
         Back to Home
       </Link>
-      <div >
-      <button className="search_button" onClick={handleInstruction}>Instructions</button>
-        <button className="search_button" onClick={handleIngredients}>Ingredients</button>
-        <h1 className="recipe_title">{details.title}</h1>
-        <img src={details.image} alt={details.title} className="recipe_image" />
+     
+        <div className="detailsContainer">
+          <div className="summary">
+          <h1 className="recipeDetail_title">{details.title}</h1>
+          <p dangerouslySetInnerHTML={{ __html: details.summary }}></p>
+          </div>
+          <div className="imageContainer">
+          <img src={details.image} alt={details.title} className="recipedetail_image" />
+          </div>
         </div>
-        
-        {showInstructions && (<div><p dangerouslySetInnerHTML={{ __html: details.summary }}></p></div>)}
-        {/* <button className="button" onClick={() => setActiveTab(ingredients)}>Ingredients</button> */}
-        
-      <p dangerouslySetInnerHTML={{ __html: details.summary }}></p>
-        {/* <p dangerouslySetInnerHTML={{ __html: details.instructions }}></p> */}
-      
-      {showIngredients && (<div>
-        <ul>
-          {details.extendedIngredients &&
-            details.extendedIngredients.map((ingredient) => (
-              <li key={ingredient.id}>{ingredient.original}</li>
-            ))}
-        </ul>
-      </div>)}
-      
-      
+        <div>
+        <button className="search_button" onClick={handleInstruction}>
+          Instructions
+        </button>
+        <button className="search_button" onClick={handleIngredients}>
+          Ingredients
+        </button>
+        </div>
+        {/* <h1 className="recipe_title">{details.title}</h1> */}
+        {/* <img src={details.image} alt={details.title} className="recipe_image" /> */}
+        {/* <p dangerouslySetInnerHTML={{ __html: details.summary }}></p> */}
+     
+
+      {showInstructions && (
+        <div>
+          <p dangerouslySetInnerHTML={{ __html: details.instructions }}></p>
+        </div>
+      )}
+      {/* <button className="button" onClick={() => setActiveTab(ingredients)}>Ingredients</button> */}
+
+      {/* <p dangerouslySetInnerHTML={{ __html: details.instructions }}></p> */}
+
+      {showIngredients && (
+        <div>
+          <ul>
+            {details.extendedIngredients &&
+              details.extendedIngredients.map((ingredient) => (
+                <li key={ingredient.id}>{ingredient.original}</li>
+              ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
